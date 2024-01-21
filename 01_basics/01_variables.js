cdf7bcab567647d08f049357809136014ef7bf37
Yogesh_13 <74135633+Yogesh311-del@users.noreply.github.com>
2024-01-21 10:54:52 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "lachhawa@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//  accountId = 1 // not allowed
+
+accountEmail = "hedn@gmail.com"
+accountPassword = "2121210"
+accountCity = "Brenjeo"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,
+accountPassword, accountCity])
+
+
+/*
+prefer not to use var.
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+// to learn and practice code hands on with keyboard and screen rather than copy and pen!
+// rely on keys. dont waste paper!
+"use strict"; //treat all JS code as newer version
+//alert (3 + 3 ) //we are using node js and not browser
+console.log(3+3) //code readability should be high
+console.log("Yogesh")
+
+let age = 3;
+
+// primitive Data Types
+// number = 2 to power 53
+// bigint -> big entries of data
+// string -> ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "yogesh");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+// undefined is type of Datatypes.
+// but null is an object.
+
