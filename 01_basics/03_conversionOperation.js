a29a5773b7d52cab0e70df2c2b7d6c6a2b99abe3
Yogesh_13 <74135633+Yogesh311-del@users.noreply.github.com>
2024-01-21 19:11:10 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,3 +30,49 @@ let someNumber = 15
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// ************* Operations ************** //
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2*2)
+// console.log(2-2)
+// console.log(2+2)
+// console.log(2**3) --> 2 power 3 
+// console.log(2/2)
+
+
+let str1 = "hello"
+let str2 = " yogesh"
+
+let str3 = str1 + str2
+console.log(str3)
+
+// ------- bit complex ----------
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); -->string ko dekh le if string pehle hai to it will be 122
+// console.log(1 + 2 + "2"); --> if like this ki string baad mein hai to pehle addition ho jaega 32
+
+
+// not much needed
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+// ----**** important ****---- //
+let x = 3
+const y = x++ // aur x++
+console.log(x);
+console.log(y);
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// dont do comparison between two different datatypes(*avoid them*)--egs are given below
+// console.log("2" > 1);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// === -> checks more effectivly
+
+console.log("2" === 2);
