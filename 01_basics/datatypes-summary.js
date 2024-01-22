1768c8d1fa81952e1d4ce93b8b898e6adb039696
Yogesh_13 <74135633+Yogesh311-del@users.noreply.github.com>
2024-01-22 14:41:50 +0000

    stack heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -45,3 +45,27 @@ console.log(typeof anotherId);
 // function                 object function
 // 100.3                    number
 // undefined                undefined
+
+
+// $$$$$$$$$$$$$$$$$$ //
+//              Memory
+// Stack (Primitive)means that you will get copy of variable declared, Heap (Non-Primitive) means that you will get reference real value. so changes(updates) done will be in real values.
+
+let myYoutubename = "yogeshdotyoutube"
+
+let anotherName = myYoutubename
+anotherName = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "yogesh@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
